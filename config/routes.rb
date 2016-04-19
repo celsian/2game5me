4be75d2ab51178cc101daa8b5050c798876d7e62
Lucas Paolella <celsian@gmail.com>
2016-04-19 12:41:27 -0700

    +home controller +bootswatch_theme +font awesome -turbo_links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'games#index'
+  root to: 'home#index'
   resource :games
+  resource :home, only: [ :index ]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
