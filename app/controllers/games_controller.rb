216e4dec7b14f69a635c1498b5701bef3d63abe1
Lucas Paolella <celsian@gmail.com>
2016-05-31 09:59:12 -0700

    Added game actions and form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,4 +5,25 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to root_path, flash: { success: "Game entry created." }
+    else
+      flash[:error] = "Error: #{@game.error_messages}"
+      render :new
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :genre, :description)
+  end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -19,4 +19,34 @@ RSpec.describe GamesController, type: :controller do
     end
   end
 
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+
+      expect(response).to render_template(:new)
+    end
+
+    it "creates a game" do
+      get :new
+
+      expect(assigns(:game)).to_not be_nil
+    end
+  end
+
+  describe "POST #create" do
+    let(:params) { { game: attributes } }
+    let(:attributes) { { title: "ok", genre: "ok", description: "ok" } }
+
+    it "redirects to root path" do
+      post :create, params
+
+      expect(response).to redirect_to root_path
+    end
+
+    it "creates the game" do
+      post :create, params
+
+      expect(assigns(:game)).to be_persisted 
+    end
+  end
 end
